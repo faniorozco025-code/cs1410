140f21f3fc800b893a9ef2e1a0bfe1ed4f0a1d75
faniorozco025-code <faniorozco025@gmail.com>
2026-01-15 05:59:04 +0000

    Finished example0102

---
+++ src/example0102.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+    double c, f;
+    cout << "Temaperature in Celcius:" << endl;
+    cin >> c;
+
+    f = 9.0 * c / 5.0 + 32;
+    cout << "Temperature in Fahrenheit:" << f << endl;
+    return 0; 
+}
+
+
